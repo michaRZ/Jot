6d93c554cd3bb6b8a181a08239f33438ef009b2a
Michaela Raines <kellar.dev@gmail.com>
2021-09-29 10:33:38 -0400

    initial commit

---
+++ Jot.WebMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Jot.WebMVC.MvcApplication" Language="C#" %>
---
+++ Jot.WebMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Jot.WebMVC.Startup))]
+namespace Jot.WebMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
